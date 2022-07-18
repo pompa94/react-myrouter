056ffd8a52fbb0f1f6952ba8ddd61c550951c977
pompa94 <crd4359@gmail.com>
2022-07-18 14:32:23 +0900

    리액트수정

--- src/pages/sample01.js
+++ src/pages/sample01.js
@@ -1,6 +1,32 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
-function sample01(){}
+import './page.css'
+
+function Sample01(){
+    return(
+        <>
+        <article id='home'>
+            <h1>상의 페이지 입니다</h1>
+            <p>대표 상품 리스트 입니다.</p>
+            <ul>
+                <li>
+                    <Link to="/list01">상의</Link>
+                </li>
+                <li>
+                    <Link to="/list02">하의</Link>
+                </li>
+                <li>
+                    <Link to="/list03">로그인</Link>
+                </li>
+                <li>
+                    <Link to="/list05">마이페이지</Link>
+                </li>
+            </ul>
+        </article>
+        </>
+    )
+}
 
 
-export default sample01
+export default Sample01
---
+++ src/pages/sample03.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+import './page.css'
+
+function Sample03(){
+    return(
+        <>
+        <article id='home'>
+            <h1>로그인 페이지 입니다</h1>
+            <p>대표 상품 리스트 입니다.</p>
+            <ul>
+                <li>
+                    <Link to="/list01">상의</Link>
+                </li>
+                <li>
+                    <Link to="/list02">하의</Link>
+                </li>
+                <li>
+                    <Link to="/list03">로그인</Link>
+                </li>
+                <li>
+                    <Link to="/list05">마이페이지</Link>
+                </li>
+            </ul>
+        </article>
+        </>
+    )
+}
+
+
+export default Sample03
